b8ffbec731777fe6a5216dd5673302f19673c756
noxlycaelum <itzryan.777@gmail.com>
2026-06-17 18:41:50 +0530

    miss me like you kiss me

--- drinkwater.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const params = new URLSearchParams(window.location.search);
-
-    if (params.get('ce') === 'prive') {
-        window.location.href = './prive.html';
-    }
-});
